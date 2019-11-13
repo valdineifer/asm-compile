654504340d1cd3f6fecc06d62cc0dcd4183317d0
Valdinei Ferreira <valdineifer@outlook.com>
2019-11-13 12:32:51 -0300

    commit rapidao

--- main.asm
+++ main.asm
@@ -1,30 +1,11 @@
-
-
-;
-; file: first.asm
-; First assembly program. This program asks for two integers as
-; input and prints out their sum.
-;
-; To create executable:
-; Using djgpp:
-; nasm -f coff first.asm
-; gcc -o first first.o driver.c asm_io.o
-;
 ; Using Linux and gcc:
-; nasm -f elf first.asm
-; gcc -o first first.o driver.c asm_io.o
-;
-; Using Borland C/C++
-; nasm -f obj first.asm
-; bcc32 first.obj driver.c asm_io.obj
+; nasm -f elf -d ELF_TYPE asm_io.asm
+; nasm -f elf main.asm
+; gcc -o main driver.c main.o asm_io.o -m32
 ;
 ; Using MS C/C++
 ; nasm -f win32 first.asm
 ; cl first.obj driver.c asm_io.obj
-;
-; Using Open Watcom
-; nasm -f obj first.asm
-; wcl386 first.obj driver.c asm_io.obj
 
 %include "asm_io.inc"
 ;
@@ -34,12 +15,17 @@ segment .data
 ;
 ; These labels refer to strings used for output
 ;
-prompt1 db    "Enter a number: ", 0       ; don't forget nul terminator
-prompt2 db    "Enter another number: ", 0
-outmsg1 db    "You entered ", 0
-outmsg2 db    " and ", 0
-outmsg3 db    ", the sum of these is ", 0
+prompt1 db    "Digite um numero: ", 0       ; don't forget nul terminator
+outmsg1 db    "NOTAS: ", 0
+outmsg2 db    " nota(s) de R$ ", 0
 
+N100  dd 100
+N50   dd 50
+N25   dd 25
+N10   dd 10
+N5    dd 5
+N2    dd 2
+N1    dd 1
 
 ;
 ; uninitialized data is put in the .bss segment
@@ -48,57 +34,32 @@ segment .bss
 ;
 ; These labels refer to double words used to store the inputs
 ;
-input1  resd 1
-input2  resd 1
-
- 
+input resd 1
 
 ;
 ; code is put in the .text segment
 ;
 segment .text
-        global  asm_main
+    global  asm_main
 asm_main:
-        enter   0,0               ; setup routine
-        pusha
+    enter   0,0             ; setup routine
+    pusha
 
-        mov     eax, prompt1      ; print out prompt
-        call    print_string
+    mov     eax, prompt1    ; print out prompt
+    call    print_string
 
-        call    read_int          ; read integer
-        mov     [input1], eax     ; store into input1
+    call    read_int        ; read integer
+    mov     [input], eax    ; store into input
 
-        mov     eax, prompt2      ; print out prompt
-        call    print_string
+    mov     eax, [input]    ; eax = dword at input
+    idiv    N100            ; edx = eax / 100
+    imul    ecx, edx, N100  ; ecx = edx * 100
+    mov     eax, ecx        ; move ecx to eax
+    sub     ebx, ecx        ; eax = eax - ecx
 
-        call    read_int          ; read integer
-        mov     [input2], eax     ; store into input2
+    call    print_nl        ; print new-line
 
-        mov     eax, [input1]     ; eax = dword at input1
-        add     eax, [input2]     ; eax += dword at input2
-        mov     ebx, eax          ; ebx = eax
-        dump_regs 1               ; dump out register values
-        dump_mem 2, outmsg1, 1    ; dump out memory
-;
-; next print out result message as series of steps
-;
-        mov     eax, outmsg1
-        call    print_string      ; print out first message
-        mov     eax, [input1]     
-        call    print_int         ; print out input1
-        mov     eax, outmsg2
-        call    print_string      ; print out second message
-        mov     eax, [input2]
-        call    print_int         ; print out input2
-        mov     eax, outmsg3
-        call    print_string      ; print out third message
-        mov     eax, ebx
-        call    print_int         ; print out sum (ebx)
-        call    print_nl          ; print new-line
-
-        popa
-        mov     eax, 0            ; return back to C
-        leave                     
-        ret
-
-
+    popa
+    mov     eax, 0          ; return back to C
+    leave                     
+    ret
