9efaca42d0aeaf59876ff4efe7e9308c23c16b73
Valdinei Ferreira <valdineifer@outlook.com>
2019-11-17 00:35:52 -0300

    [atv-notas] finished

--- main.asm
+++ main.asm
@@ -4,8 +4,10 @@
 ; gcc -o main driver.c main.o asm_io.o -m32
 ;
 ; Using MS C/C++
-; nasm -f win32 first.asm
-; cl first.obj driver.c asm_io.obj
+; nasm -f win32 main.asm
+; cl main.obj driver.c asm_io.obj
+;
+; Author: Valdinei Ferreira
 
 %include "asm_io.inc"
 ;
@@ -15,17 +17,11 @@ segment .data
 ;
 ; These labels refer to strings used for output
 ;
-prompt1 db    "Digite um numero: ", 0       ; don't forget nul terminator
-outmsg1 db    "NOTAS: ", 0
-outmsg2 db    " nota(s) de R$ ", 0
+prompt1 db  "Digite um numero: ", 0       ; don't forget nul terminator
+outmsg1 db  "NOTAS: ", 0
+outmsg2 db  " nota(s) de R$ ", 0
 
-N100  dd 100
-N50   dd 50
-N25   dd 25
-N10   dd 10
-N5    dd 5
-N2    dd 2
-N1    dd 1
+ncem    db  100, 0
 
 ;
 ; uninitialized data is put in the .bss segment
@@ -41,6 +37,7 @@ input resd 1
 ;
 segment .text
     global  asm_main
+
 asm_main:
     enter   0,0             ; setup routine
     pusha
@@ -52,16 +49,122 @@ asm_main:
     mov     [input], eax    ; store into input
 
     mov     eax, [input]    ; eax = dword at input
-	mov		ecx, N100
+    cdq                     ; initialize edx by sign extension, Convert Double to Quad word
+	mov		ecx, 100
     idiv    ecx            	; edx = eax / ecx
-    imul    ecx, edx, N100  ; ecx = edx * 100 -> ecx = (num / 100) * 100 
-    ; mov     eax, ecx        ; move ecx to eax
+    imul    ecx, eax, 100  ; ecx = edx * 100 -> ecx = (num / 100) * 100
+    mov     ebx, eax
     sub     eax, ecx        ; eax = eax - ecx -> num = num - (num / 100) * 100
 
-	; mov     eax, ecx
+	mov     eax, outmsg1
+	call    print_string
+    call    print_nl        ; print new-line
+    
+	mov     eax, ebx
 	call    print_int
 
+    mov     eax, outmsg2
+    call    print_string
+
+    mov     eax, 100
+    call    print_int
     call    print_nl        ; print new-line
+
+
+    ; PARA NOTAS DE 50
+    mov     eax, edx
+    cdq                     ; initialize edx by sign extension, Convert Double to Quad word
+	mov		ecx, 50
+    idiv    ecx            	; edx = eax / ecx
+
+    mov     eax, eax
+	call    print_int
+
+    mov     eax, outmsg2
+    call    print_string
+
+    mov     eax, 50
+    call    print_int
+    call    print_nl        ; print new-line
+
+
+    ; PARA NOTAS DE 20
+    mov     eax, edx
+    cdq                     ; initialize edx by sign extension, Convert Double to Quad word
+	mov		ecx, 20
+    idiv    ecx            	; edx = eax / ecx
+
+    mov     eax, eax
+	call    print_int
+
+    mov     eax, outmsg2
+    call    print_string
+
+    mov     eax, 20
+    call    print_int
+    call    print_nl        ; print new-line
+
+
+    ; PARA NOTAS DE 10
+    mov     eax, edx
+    cdq                     ; initialize edx by sign extension, Convert Double to Quad word
+	mov		ecx, 10
+    idiv    ecx            	; edx = eax / ecx
+
+    mov     eax, eax
+	call    print_int
+
+    mov     eax, outmsg2
+    call    print_string
+
+    mov     eax, 10
+    call    print_int
+    call    print_nl        ; print new-line
+
+
+    ; PARA NOTAS DE 5
+    mov     eax, edx
+    cdq                     ; initialize edx by sign extension, Convert Double to Quad word
+	mov		ecx, 5
+    idiv    ecx            	; edx = eax / ecx
+
+	call    print_int
+
+    mov     eax, outmsg2
+    call    print_string
+
+    mov     eax, 5
+    call    print_int
+    call    print_nl        ; print new-line
+
+
+    ; PARA NOTAS DE 2
+    mov     eax, edx
+    cdq                     ; initialize edx by sign extension, Convert Double to Quad word
+	mov		ecx, 2
+    idiv    ecx            	; edx = eax / ecx
+
+	call    print_int
+
+    mov     eax, outmsg2
+    call    print_string
+
+    mov     eax, 2
+    call    print_int
+    call    print_nl        ; print new-line
+
+
+    ; PARA NOTAS DE 1
+    mov     eax, edx
+	call    print_int
+
+    mov     eax, outmsg2
+    call    print_string
+
+    mov     eax, 1
+    call    print_int
+    call    print_nl        ; print new-line
+
 
     popa
     mov     eax, 0          ; return back to C
